81c6ee77c8536c3dec667808b7b4195719d48b0c
qvaseel <vasilina.makarkina@gmail.com>
2026-09-20 04:41:57 +0700

    first commit

---
+++ src/pages/IndexPage/IndexPage.tsx
@@ -0,0 +1,37 @@
+import { Section, Cell, Image, List, Badge } from "@telegram-apps/telegram-ui";
+import type { FC } from "react";
+
+import { Link } from "@/components/Link/Link.tsx";
+import { Page } from "@/components/Page.tsx";
+
+import tonSvg from "./ton.svg";
+
+export const IndexPage: FC = () => {
+  return (
+    <Page back={false}>
+      <List>
+        <Badge type="number">ТЕСТ</Badge>
+        <Section
+          header="Application Launch Data"
+          footer="These pages help developer to learn more about current launch information"
+        >
+          <Link to="/init-data">
+            <Cell subtitle="User data, chat information, technical data">
+              Init Data
+            </Cell>
+          </Link>
+          <Link to="/launch-params">
+            <Cell subtitle="Platform identifier, Mini Apps version, etc.">
+              Launch Parameters
+            </Cell>
+          </Link>
+          <Link to="/theme-params">
+            <Cell subtitle="Telegram application palette information">
+              Theme Parameters
+            </Cell>
+          </Link>
+        </Section>
+      </List>
+    </Page>
+  );
+};
